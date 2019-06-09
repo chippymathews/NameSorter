4623edf75f077f5c412921250334f6a6d7b825f6
chippymathews <chippy.mathews91@gmail.com>
2019-06-09 10:17:28 +0300

    Commit

---
+++ name-sorter-Test/UnitTestNameSorter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using name_sorter;
+
+namespace name_sorter_Test
+{
+    [TestClass]
+    public class UnitTestNameSorter
+    {
+        [TestMethod]
+        public void TestMethod_CheckForNamesWithPositiveCase_AtmostThreeNames()
+        {
+            string[] expectedResult = new string[] { "Marin Alvarez", "Adonis Julius Archer", "Beau Tristan Bentley", "Hunter Uriah Mathew Clarke", "Leo Gardner", "Vaughn Lewis", "London Lindsey", "Mikayla Lopez", "Janet Parsons", "Frankie Conner Ritter", "Shelby Nathan Yoder" };
+            string[] actualStringArray = new string[] { "Janet Parsons", "Vaughn Lewis", "Adonis Julius Archer", "Shelby Nathan Yoder", "Marin Alvarez", "London Lindsey", "Beau Tristan Bentley", "Leo Gardner", "Hunter Uriah Mathew Clarke", "Mikayla Lopez", "Frankie Conner Ritter" };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(actualResult,expectedResult);
+        }
+        [TestMethod]
+        public void TestMethod_CheckforNamesWithPositiveCase_FirstAndLastNamesOnly()
+        {
+            string[] expectedResult = new string[] { "Marin  Alvarez", "Leo  Gardner", "Vaughn  Lewis", "London  Lindsey", "Mikayla  Lopez", "Janet  Parsons" };
+            string[] actualStringArray = new string[] { "Janet Parsons", "Vaughn Lewis", "Marin Alvarez", "London Lindsey", "Leo Gardner", "Mikayla Lopez" };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod_CheckforNamesWithPositiveCase_NamesWithFirstNameOnly()
+        {
+            string[] expectedResult = new string[] { "Janet", "Leo", "London", "Marin", "Mikayla", "Vaughn" };
+            string[] actualStringArray = new string[] { "Janet", "Vaughn", "Marin", "London", "Leo", "Mikayla" };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void TestMethod_CheckforNamesWithPositiveCase_NamesWithNullAndWhiteSpaceValues()
+        {
+            string[] expectedResult = new string[] { "Marin  Alvarez", "Adonis Julius Archer", "Beau Tristan Bentley", "Hunter Uriah Mathew Clarke", "Leo  Gardner", "Vaughn  Lewis", "London  Lindsey", "Mikayla  Lopez", "Janet  Parsons", "Frankie Conner Ritter", "Shelby Nathan Yoder" };
+            string[] actualStringArray = new string[] { "Janet Parsons", "Vaughn Lewis", "Adonis Julius Archer", "Shelby Nathan Yoder", "Marin Alvarez", "London Lindsey", "Beau Tristan Bentley", "Leo Gardner", "Hunter Uriah Mathew Clarke", "Mikayla Lopez", "Frankie Conner Ritter", " ", "",null };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void TestMethod_CheckforNamesWithPositiveCase_NamesWithNumericValues()
+        {
+            string[] expectedResult = new string[] { "Marin  Alvarez", "Adonis Julius Archer", "Beau Tristan Bentley", "Hunter Uriah Mathew Clarke", "Leo  Gardner", "Vaughn  Lewis", "London  Lindsey", "Mikayla  Lopez", "Janet  Parsons", "Frankie Conner Ritter", "Shelby Nathan Yoder" };
+            string[] actualStringArray = new string[] { "Janet Parsons", "Vaughn Lewis", "Adonis Julius Archer", "Shelby Nathan Yoder", "Marin Alvarez", "London Lindsey", "Beau Tristan Bentley", "Leo Gardner", "Hunter Uriah Mathew Clarke", "Mikayla Lopez", "Frankie Conner Ritter", "1", "2", "3" };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void TestMethod_CheckforNamesWithPositiveCase_NamesWithSpecialCharacters()
+        {
+            string[] expectedResult = new string[] { "Marin  Alvarez", "Adonis Julius Archer", "Beau Tristan Bentley", "Hunter Uriah Mathew Clarke", "Leo  Gardner", "Vaughn  Lewis", "London  Lindsey", "Mikayla  Lopez", "Janet  Parsons", "Frankie Conner Ritter", "Shelby Nathan Yoder" };
+            string[] actualStringArray = new string[] { "Janet Parsons", "Vaughn Lewis", "Adonis Julius Archer", "Shelby Nathan Yoder", "Marin Alvarez", "London Lindsey", "Beau Tristan Bentley", "Leo Gardner", "Hunter Uriah Mathew Clarke", "Mikayla Lopez", "Frankie Conner Ritter", "#", "$", "&" };
+            NameSorterClass sortObject = new NameSorterClass();
+            string[] actualResult = sortObject.sortArrayString(actualStringArray);
+            CollectionAssert.Equals(expectedResult, actualResult);
+        }
+    }
+}
---
+++ name-sorter/NameSorterClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace name_sorter
+{
+    public class NameSorterClass
+    {
+        public string[] sortArrayString(string[] stringArray)
+        {
+            int outPut;
+            var regex = new Regex(@"(?i)^[a-z.,\s]+$");
+            List<Person> personlist = new List<Person>();
+            foreach (string name in stringArray)
+            {
+                string firstname = string.Empty;
+                string lastname = string.Empty;
+                string middlename = string.Empty;
+                Console.WriteLine(name);
+                if (!string.IsNullOrEmpty(name) && !int.TryParse(name, out outPut) && regex.IsMatch(name))
+                {
+                    var arrNames = name.Trim().Split(' ');
+
+                    if (arrNames.Length == 1)
+                    {
+                        if (!string.IsNullOrWhiteSpace(arrNames.FirstOrDefault()))
+                        {
+                            lastname = arrNames.FirstOrDefault();
+                            //Console.WriteLine("lastname: " + lastname);
+                        }
+                    }
+                    else if (arrNames.Length > 0)
+                    {
+                        if (!string.IsNullOrWhiteSpace(arrNames.FirstOrDefault()))
+                        {
+                            firstname = arrNames.FirstOrDefault();
+                            //Console.WriteLine("firstname: " + firstname);
+                        }
+                    }
+                    if (arrNames.Length > 1)
+                    {
+                        lastname = arrNames.LastOrDefault();
+                        //Console.WriteLine("lastname: " + lastname);
+                    }
+                    if (arrNames.Length > 2)
+                    {
+                        middlename = string.Join(" ", arrNames, 1, arrNames.Length - 2);
+                        //Console.WriteLine("middlename: " + middlename);
+                    }
+                    if (!string.IsNullOrEmpty(firstname) || !string.IsNullOrEmpty(middlename) || !string.IsNullOrEmpty(lastname))
+                    {
+                        Person p = new Person() { firstname = firstname, middlename = middlename, lastname = lastname, };
+                        personlist.Add(p);
+                    }
+                }
+            }
+            personlist = personlist.OrderBy(p => p.lastname).ThenBy(p => p.middlename).ThenBy(p => p.firstname).ToList();
+            List<string> names = new List<string>();
+            foreach (Person p in personlist)
+            {
+                string fullname = string.Concat(p.firstname, " ", p.middlename, " ", p.lastname).Trim();
+                names.Add(fullname);
+            }
+
+            string[] sortedStrArray = names.ToArray();
+            return sortedStrArray;
+        }
+        public static void Main(string[] args)
+        {
+            //string path = args[0].ToString();
+            string[] readText = File.ReadAllLines(@".\unsorted-names-list.txt");
+
+            NameSorterClass sorter = new NameSorterClass();
+            String[] sortedlist = sorter.sortArrayString(readText);
+            Console.WriteLine("\nOUTPUT:\n");
+            foreach (string str in sortedlist)
+                Console.WriteLine(str);
+            File.WriteAllLines(@".\sorted-names-list.txt", sortedlist, Encoding.UTF8);
+            Console.ReadKey();
+        }
+        public class Person
+        {
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string middlename { get; set; }
+        }
+    }
+}
